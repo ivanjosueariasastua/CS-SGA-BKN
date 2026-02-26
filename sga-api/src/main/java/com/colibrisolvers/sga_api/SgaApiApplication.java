8b39c59b13e36a8295f473293abac563b5b2d936
Iván Josué Arias Astúa <53876880+ivanjosueariasastua@users.noreply.github.com>
2026-02-26 00:07:28 -0600

    Initial commit

---
+++ sga-api/src/main/java/com/colibrisolvers/sga_api/SgaApiApplication.java
@@ -0,0 +1,13 @@
+package com.colibrisolvers.sga_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgaApiApplication.class, args);
+	}
+
+}
---
+++ sga-api/src/test/java/com/colibrisolvers/sga_api/SgaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.colibrisolvers.sga_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
